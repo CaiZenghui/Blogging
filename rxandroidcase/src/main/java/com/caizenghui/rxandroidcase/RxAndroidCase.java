33e31294588c952966279da0fe245a513cc4ad31
caizenghui <zenghui.cai@dianping.com>
2016-05-26 19:30:04 +0800

    udpate RxAndroidCase

--- rxandroidcase/src/main/java/com/caizenghui/rxandroidcase/RxAndroidCase.java
+++ rxandroidcase/src/main/java/com/caizenghui/rxandroidcase/RxAndroidCase.java
@@ -3,6 +3,7 @@ package com.caizenghui.rxandroidcase;
 
 import android.util.Log;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
@@ -10,6 +11,7 @@ import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action0;
 import rx.functions.Action1;
+import rx.functions.Func0;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
@@ -19,18 +21,121 @@ import rx.schedulers.Schedulers;
 public class RxAndroidCase {
 
     public static void main(String[] args) {
-        testd();
+        testj();
     }
 
-    private static void testd() {
-        Observable observable1 = Observable.range(1,1000,Schedulers.newThread());
-        Observable observable2 = Observable.range(1000,1000,Schedulers.newThread());
-        Observable observable3 = Observable.range(2000,1000,Schedulers.newThread());
-        Observable.concat(observable1,observable2,observable3)
+    private static void testj() {
+    }
+
+    private static void testi() {
+        Observable observable1 = Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                Log.d("RxAndroidCase", "observable1--->" + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                subscriber.onNext(0);
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.newThread());
+        Observable observable2 = Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                Log.d("RxAndroidCase", "observable2--->" + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                subscriber.onNext(1);
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.io());
+
+        Observable.merge(observable1,observable2)
+                .buffer(2)
                 .subscribe(new Action1() {
                     @Override
                     public void call(Object o) {
-                        Log.d("RxAndroidCase", Thread.currentThread().getName()+"-->"+o.toString());
+                        Log.d("RxAndroidCase", o.toString());
+                    }
+                });
+    }
+
+    /**
+     <------>
+     <------>
+     <------>
+     <------>
+     */
+    private static void testh() {
+        Observable.create(new Observable.OnSubscribe<Object>() {
+            @Override
+            public void call(Subscriber<? super Object> subscriber) {
+                System.out.println("<------>");
+                subscriber.onError(null);
+            }
+        }).retry(3)
+                .subscribe(new Action1<Object>() {
+                    @Override
+                    public void call(Object o) {
+                        System.out.println("------>");
+                    }
+                });
+    }
+
+    /**
+     * [0, 1, 2] skip --> 3
+     * [4, 5, 6]
+     */
+    private static void testg() {
+        Observable.just(0, 1, 2, 3, 4, 5, 6).buffer(3, 4)
+                .subscribe(new Action1<List<Integer>>() {
+                    @Override
+                    public void call(List<Integer> integers) {
+                        System.out.println(integers);
+                    }
+                });
+    }
+
+    /**
+     * [0, 1, 2]  skip -->0,1
+     * [2, 3, 4]  skip -->1,3
+     * [4, 5, 6]  skip -->4,5
+     * [6]
+     */
+    private static void testf() {
+        Observable.just(0, 1, 2, 3, 4, 5, 6).buffer(3, 2)
+                .subscribe(new Action1<List<Integer>>() {
+                    @Override
+                    public void call(List<Integer> integers) {
+                        System.out.println(integers);
+                    }
+                });
+    }
+
+    private static void teste() {
+        Observable.just(0, 1, 2, 3, 4, 5, 6).buffer(2)
+                .subscribe(new Action1<List<Integer>>() {
+                    @Override
+                    public void call(List<Integer> integers) {
+                        System.out.println(integers);
+                    }
+                });
+    }
+
+    private static void testd() {
+        Observable observable1 = Observable.range(1, 1000, Schedulers.newThread());
+        Observable observable2 = Observable.range(1000, 1000, Schedulers.newThread());
+        Observable observable3 = Observable.range(2000, 1000, Schedulers.newThread());
+        Observable.concat(observable1, observable2, observable3)
+                .subscribe(new Action1() {
+                    @Override
+                    public void call(Object o) {
+                        Log.d("RxAndroidCase", Thread.currentThread().getName() + "-->" + o.toString());
                     }
                 });
     }
@@ -39,14 +144,14 @@ public class RxAndroidCase {
      * 线程交替输出,各自在各自线程输出;
      */
     private static void testc() {
-        Observable observable1 = Observable.range(1,1000,Schedulers.newThread());
-        Observable observable2 = Observable.range(1000,1000,Schedulers.newThread());
-        Observable observable3 = Observable.range(2000,1000,Schedulers.newThread());
-        Observable.merge(observable1,observable2,observable3)
+        Observable observable1 = Observable.range(1, 1000, Schedulers.newThread());
+        Observable observable2 = Observable.range(1000, 1000, Schedulers.newThread());
+        Observable observable3 = Observable.range(2000, 1000, Schedulers.newThread());
+        Observable.merge(observable1, observable2, observable3)
                 .subscribe(new Action1() {
                     @Override
                     public void call(Object o) {
-                        Log.d("RxAndroidCase", Thread.currentThread().getName()+"-->"+o.toString());
+                        Log.d("RxAndroidCase", Thread.currentThread().getName() + "-->" + o.toString());
                     }
                 });
     }
@@ -55,25 +160,25 @@ public class RxAndroidCase {
      * 第一个线程先输出,第二个线程后输出,各自在各自线程输出;
      */
     private static void testb() {
-        Observable observable1 = Observable.range(1,20,Schedulers.newThread());
-        Observable observable2 = Observable.range(3,40,Schedulers.io());
-        Observable.concat(observable1,observable2)
+        Observable observable1 = Observable.range(1, 20, Schedulers.newThread());
+        Observable observable2 = Observable.range(3, 40, Schedulers.io());
+        Observable.concat(observable1, observable2)
                 .subscribe(new Action1() {
                     @Override
                     public void call(Object o) {
-                        Log.d("RxAndroidCase", Thread.currentThread().getName()+"-->"+o.toString());
+                        Log.d("RxAndroidCase", Thread.currentThread().getName() + "-->" + o.toString());
                     }
                 });
     }
 
     private static void testa() {
-        Observable observable1 = Observable.interval(0,2, TimeUnit.SECONDS,Schedulers.io());
-        Observable observable2 = Observable.interval(0,3,TimeUnit.SECONDS,Schedulers.newThread());
-        Observable.concat(observable1,observable2)
+        Observable observable1 = Observable.interval(0, 2, TimeUnit.SECONDS, Schedulers.io());
+        Observable observable2 = Observable.interval(0, 3, TimeUnit.SECONDS, Schedulers.newThread());
+        Observable.concat(observable1, observable2)
                 .subscribe(new Action1() {
                     @Override
                     public void call(Object o) {
-                        Log.d("RxAndroidCase", Thread.currentThread().getName()+"-->"+o.toString());
+                        Log.d("RxAndroidCase", Thread.currentThread().getName() + "-->" + o.toString());
                     }
                 });
     }
@@ -82,13 +187,13 @@ public class RxAndroidCase {
      * 两个线程交替输出,各自在各自线程输出;
      */
     private static void test16() {
-        Observable observable1 = Observable.interval(0,2, TimeUnit.SECONDS,Schedulers.io());
-        Observable observable2 = Observable.interval(0,3,TimeUnit.SECONDS,Schedulers.newThread());
-        Observable.merge(observable1,observable2)
+        Observable observable1 = Observable.interval(0, 2, TimeUnit.SECONDS, Schedulers.io());
+        Observable observable2 = Observable.interval(0, 3, TimeUnit.SECONDS, Schedulers.newThread());
+        Observable.merge(observable1, observable2)
                 .subscribe(new Action1() {
                     @Override
                     public void call(Object o) {
-                        Log.d("RxAndroidCase", Thread.currentThread().getName()+"-->"+o.toString());
+                        Log.d("RxAndroidCase", Thread.currentThread().getName() + "-->" + o.toString());
                     }
                 });
     }
@@ -108,7 +213,7 @@ public class RxAndroidCase {
                 subscriber.onCompleted();
             }
         });
-        Observable.concat(observable1,observable2)
+        Observable.concat(observable1, observable2)
                 .subscribe(new Action1() {
                     @Override
                     public void call(Object o) {
@@ -123,7 +228,7 @@ public class RxAndroidCase {
                 .map(new Func1<Integer, String>() {
                     @Override
                     public String call(Integer integer) {
-                        Log.d("RxAndroidCase", "map--->" +Thread.currentThread().getName());
+                        Log.d("RxAndroidCase", "map--->" + Thread.currentThread().getName());
                         return (integer + 5) + "";
                     }
                 })
@@ -131,8 +236,8 @@ public class RxAndroidCase {
                 .flatMap(new Func1<String, Observable<String>>() {
                     @Override
                     public Observable<String> call(String s) {
-                        Log.d("RxAndroidCase", "flatMap--->" +Thread.currentThread().getName());
-                        return Observable.just(s+"--->");
+                        Log.d("RxAndroidCase", "flatMap--->" + Thread.currentThread().getName());
+                        return Observable.just(s + "--->");
                     }
                 })
                 .subscribeOn(Schedulers.newThread())
@@ -140,7 +245,7 @@ public class RxAndroidCase {
                 .doOnSubscribe(new Action0() {
                     @Override
                     public void call() {
-                        Log.d("RxAndroidCase", "doOnSubscribe--->" +Thread.currentThread().getName());
+                        Log.d("RxAndroidCase", "doOnSubscribe--->" + Thread.currentThread().getName());
                     }
                 })
                 .subscribeOn(Schedulers.computation())
