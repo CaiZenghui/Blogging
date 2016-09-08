1d28cd419137fe2bd8be0fbe6af6b8279dd9e435
caizenghui <caizenghui@wifi.com>
2016-09-08 17:03:25 +0800

    update ndk_monitor

--- ndkmonitor/src/main/java/com/wifi/ndkmonitor/NdkMonitorMainActivity.java
+++ ndkmonitor/src/main/java/com/wifi/ndkmonitor/NdkMonitorMainActivity.java
@@ -8,6 +8,7 @@ import com.example.ndkmonitor.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 
 public class NdkMonitorMainActivity extends Activity {
 
@@ -17,24 +18,28 @@ public class NdkMonitorMainActivity extends Activity {
 		setContentView(R.layout.ndk_monitor_activity_main);
 
 		File file = new File("/mnt/sdcard/breakpad");
+		File externalStorageDirectory = Environment.getExternalStorageDirectory();
+		File dir = new File(externalStorageDirectory,"breakpad");
+		dir.mkdirs();
 		file.mkdirs();
 		final JniUtil jniUtil = new JniUtil();
-		new NdkMonitor().setNativeCrashDir("/mnt/sdcard/breakpad");
+		new NdkMonitor().setNativeCrashDir(dir.getAbsolutePath());
+//		String jniStr = jniUtil.crash();
+//		findViewById(R.id.tv).postDelayed(new Runnable() {
+//
+//			@Override
+//			public void run() {
+//				// System.loadLibrary("ndk_monitor");
+//
+//			}
+//
+//		}, 2000);
+
 		findViewById(R.id.tv).postDelayed(new Runnable() {
 
 			@Override
 			public void run() {
-				// System.loadLibrary("ndk_monitor");
-
-			}
-
-		}, 2000);
-
-		findViewById(R.id.tv).postDelayed(new Runnable() {
-
-			@Override
-			public void run() {
-//				String jniStr = jniUtil.crash();
+				String jniStr = jniUtil.crash();
 			}
 
 		}, 2000);
